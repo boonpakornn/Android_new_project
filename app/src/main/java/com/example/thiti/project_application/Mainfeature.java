3fee1962c02b269ba272be800aaf9bfb9cb8622c
Boonbpknz <37540958+Boonbpknz@users.noreply.github.com>
2018-04-28 19:45:41 +0700

    Complete Map
    adding markers

--- app/src/main/java/com/example/thiti/project_application/Mainfeature.java
+++ app/src/main/java/com/example/thiti/project_application/Mainfeature.java
@@ -47,7 +47,7 @@ public class Mainfeature extends AppCompatActivity {
         calendarView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(Mainfeature.this, Calendar.class));
+                startActivity(new Intent(Mainfeature.this, MapsActivity.class));
             }
         });
     }
--- app/src/main/java/com/example/thiti/project_application/MapsActivity.java
+++ app/src/main/java/com/example/thiti/project_application/MapsActivity.java
@@ -2,12 +2,16 @@ package com.example.thiti.project_application;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -40,7 +44,70 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng muict = new LatLng(13.7941886, 100.3248333);
-        mMap.addMarker(new MarkerOptions().position(muict).title("Marker in MUICT"));
+        mMap.addMarker(new MarkerOptions().position(muict).title("Faculty of ICT"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(muict));
+
+        LatLng socialcanteen = new LatLng(13.7928404, 100.3248672);
+        mMap.addMarker(new MarkerOptions().position(socialcanteen).title("Social Fac. Canteen"));
+
+        LatLng mlc = new LatLng(13.7939351, 100.3211026);
+        mMap.addMarker(new MarkerOptions().position(mlc).title("MU learning Center"));
+
+        LatLng salayastore = new LatLng(13.7933321,100.3227543);
+        mMap.addMarker(new MarkerOptions().position(salayastore).title("Salaya Store"));
+
+        LatLng tramstation = new LatLng(13.7947309, 100.3188644);
+        mMap.addMarker(new MarkerOptions().position(tramstation).title("Tram Station"));
+
+        LatLng princemahidolhall = new LatLng(13.7895719, 100.3212429);
+        mMap.addMarker(new MarkerOptions().position(princemahidolhall).title("Prince Mahidol Hall"));
+
+        LatLng seven = new LatLng(13.7962007, 100.3257342);
+        mMap.addMarker(new MarkerOptions().position(seven).title("Seven Eleven"));
+
+        mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+
+
+            @Override
+            // Return null here, so that getInfoContents() is called next.
+            public View getInfoWindow(Marker arg0) {
+                return null;
+            }
+
+            @Override
+            public View getInfoContents(Marker marker) {
+                View infoWindow = getLayoutInflater().inflate(R.layout.info_map, null);
+
+
+                TextView title = ((TextView) infoWindow.findViewById(R.id.textViewName));
+                title.setText(marker.getTitle());
+
+
+                TextView snippet = ((TextView) infoWindow.findViewById(R.id.textViewSnippet));
+                snippet.setText(marker.getSnippet());
+
+                ImageView imageView = (ImageView) infoWindow.findViewById(R.id.imageView);
+                imageView.setImageResource(R.drawable.laptop);
+                if ("Social Fac. Canteen".equals(marker.getTitle())) {
+                    imageView.setImageResource(R.drawable.food);
+                }
+                else if("MU learning Center".equals(marker.getTitle())) {
+                    imageView.setImageResource(R.drawable.learning);
+                }
+                else if("Salaya Store".equals(marker.getTitle())) {
+                    imageView.setImageResource(R.drawable.store);
+                }
+                else if("Prince Mahidol Hall".equals(marker.getTitle())) {
+                    imageView.setImageResource(R.drawable.stage);
+                }
+                else if("Seven Eleven".equals(marker.getTitle())) {
+                    imageView.setImageResource(R.drawable.seven);
+                }
+
+                return infoWindow;
+            }
+        });
     }
+
+
 }
