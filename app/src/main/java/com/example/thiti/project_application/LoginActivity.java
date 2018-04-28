9e2238bb132f7c7bec5410a9f12167aba505e65f
Thitiwut Chamornmarn <thitiwut.chm@gmail.com>
2018-04-29 04:50:37 +0700

    Update
    eiei

--- app/src/main/java/com/example/thiti/project_application/LoginActivity.java
+++ app/src/main/java/com/example/thiti/project_application/LoginActivity.java
@@ -184,7 +184,6 @@ public class LoginActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithEmail:success");
-                            //FirebaseUser user = mAuth.getCurrentUser();
 
                             startActivity(new Intent(LoginActivity.this, Mainfeature.class));
 
