bdc6db6ac35835fc47a9c508d765b56dcb61d811
Thitiwut Chamornmarn <thitiwut.chm@gmail.com>
2018-04-28 12:51:18 +0700

    Her~
    wtf

--- app/src/main/java/com/example/thiti/project_application/NewsFragment.java
+++ app/src/main/java/com/example/thiti/project_application/NewsFragment.java
@@ -13,8 +13,11 @@ import android.support.v7.widget.RecyclerView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -26,12 +29,23 @@ public class NewsFragment extends Fragment {
     //a list to store all the products
     List<informationdetail> productList;
 
-    //the recyclerview
+
     RecyclerView recyclerView;
     private FirebaseAuth mAuth;
     private FirebaseDatabase firebaseDatabase;
+    private  FirebaseAuth.AuthStateListener mAuthListener;
+
     private DatabaseReference databaseref;
     private FirebaseStorage firebaseStorage;
+
+    private String  currentId;
+    private String currenttitle;
+
+    private String  shortDes;
+    private String longDes;
+
+    private String imgLink;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -43,18 +57,51 @@ public class NewsFragment extends Fragment {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
 
-
+        firebaseDatabase = FirebaseDatabase.getInstance();
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser user = mAuth.getCurrentUser();
        // userId = user.getUid();
 
-        databaseref = FirebaseDatabase.getInstance().getReference().child()
-        firebaseStorage =  FirebaseStorage.getInstance();
-        stora
+        databaseref = FirebaseDatabase.getInstance().getReference().child("Information").child("News");
+        //firebaseStorage =  FirebaseStorage.getInstance();
+
         //initializing the productlist
         productList = new ArrayList<>();
 
 
+        databaseref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+
+
+                currentId = dataSnapshot.child("ID").getValue().toString().trim();
+                Integer.parseInt(currentId);
+                currenttitle = dataSnapshot.child("Topic").getValue().toString().trim();
+                //currentId = dataSnapshot.child().getValue().toString().trim();
+                longDes  = dataSnapshot.child("shortDes").getValue().toString().trim();
+
+                shortDes  = dataSnapshot.child("LongDes").getValue().toString().trim();
+                imgLink = dataSnapshot.child("LinkImg").getValue().toString().trim();
+                Integer.parseInt(imgLink);
+
+
+
+                productList.add(new informationdetail(Integer.parseInt(currentId),currenttitle,shortDes,longDes, Integer.parseInt(imgLink)
+ ));
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        //productList.add(new informationdetail());
+
+
+        /*
         //adding some items to our list
                productList.add(
                 new informationdetail(
@@ -72,7 +119,7 @@ public class NewsFragment extends Fragment {
                         "Wowwwwwwwww",
                         android.R.drawable.ic_input_delete));
 
-
+        */
         //creating recyclerview adapter
         Adapter adapter = new Adapter(view.getContext(), productList);
 
